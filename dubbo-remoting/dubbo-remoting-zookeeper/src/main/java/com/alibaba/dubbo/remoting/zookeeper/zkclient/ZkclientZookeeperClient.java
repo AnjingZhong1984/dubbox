75d6235efcd83fdbd3d76d30c5757569ebdffac9
Silence <zhong_anjing@163.com>
2017-07-20 15:34:59 +0800

    update to spring boot 1.5.4, and update some package

--- dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/com/alibaba/dubbo/remoting/zookeeper/zkclient/ZkclientZookeeperClient.java
+++ dubbo-remoting/dubbo-remoting-zookeeper/src/main/java/com/alibaba/dubbo/remoting/zookeeper/zkclient/ZkclientZookeeperClient.java
@@ -1,8 +1,10 @@
 package com.alibaba.dubbo.remoting.zookeeper.zkclient;
 
-import java.util.List;
-
 import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.remoting.zookeeper.ChildListener;
+import com.alibaba.dubbo.remoting.zookeeper.StateListener;
+import com.alibaba.dubbo.remoting.zookeeper.support.AbstractZookeeperClient;
 import org.I0Itec.zkclient.IZkChildListener;
 import org.I0Itec.zkclient.IZkStateListener;
 import org.I0Itec.zkclient.ZkClient;
@@ -10,10 +12,7 @@ import org.I0Itec.zkclient.exception.ZkNoNodeException;
 import org.I0Itec.zkclient.exception.ZkNodeExistsException;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
 
-import com.alibaba.dubbo.common.URL;
-import com.alibaba.dubbo.remoting.zookeeper.ChildListener;
-import com.alibaba.dubbo.remoting.zookeeper.StateListener;
-import com.alibaba.dubbo.remoting.zookeeper.support.AbstractZookeeperClient;
+import java.util.List;
 
 public class ZkclientZookeeperClient extends AbstractZookeeperClient<IZkChildListener> {
 
@@ -28,6 +27,7 @@ public class ZkclientZookeeperClient extends AbstractZookeeperClient<IZkChildLis
                 url.getParameter(Constants.SESSION_TIMEOUT_KEY, Constants.DEFAULT_SESSION_TIMEOUT),
                 url.getParameter(Constants.TIMEOUT_KEY, Constants.DEFAULT_REGISTRY_CONNECT_TIMEOUT));
 		client.subscribeStateChanges(new IZkStateListener() {
+			@Override
 			public void handleStateChanged(KeeperState state) throws Exception {
 				ZkclientZookeeperClient.this.state = state;
 				if (state == KeeperState.Disconnected) {
@@ -36,8 +36,15 @@ public class ZkclientZookeeperClient extends AbstractZookeeperClient<IZkChildLis
 					stateChanged(StateListener.CONNECTED);
 				}
 			}
+
+			@Override
 			public void handleNewSession() throws Exception {
 				stateChanged(StateListener.RECONNECTED);
+			}
+
+			@Override
+			public void handleSessionEstablishmentError(Throwable error) throws Exception {
+				error.printStackTrace();
 			}
 		});
 	}
